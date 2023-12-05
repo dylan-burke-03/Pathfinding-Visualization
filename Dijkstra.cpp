4d1d1a3d0f22880d95a2e5f13c0c4087a528e059
Samgustafson1 <152348508+Samgustafson1@users.noreply.github.com>
2023-12-04 23:33:35 -0500

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -12,17 +12,21 @@ public:
     double distance;
     Node* parent;
 
+    //Node structure to allow for input of adjacency matrix with nodes
     Node(int x, int y, Node* defaultParent) : x(x), y(y), prevVisited(false), distance(INT_MAX), parent(defaultParent) {}
 };
 
-struct CompareNodes {
+//Function to help the priority queue ensure it is prioritized based on distance
+struct CompareNodes { 
     bool operator()(const Node& a, const Node& b) const {
         return a.distance > b.distance;
     }
 };
 
+//Dijkstra's algorithm function that takes in the adjacency matrix of nodes
+//Also takes in specified start and end nodes
 void dijkstra(const vector<vector<Node>>& adjacencyMatrix, vector<vector<Node>>& grid, Node& begin, Node& end) {
-    priority_queue<Node, vector<Node>, CompareNodes> pq;
+    priority_queue<Node, vector<Node>, CompareNodes> pq; //Priority queue that prioritizes based on distance
     pq.push(begin);
     begin.distance = 0;
 
@@ -31,22 +35,22 @@ void dijkstra(const vector<vector<Node>>& adjacencyMatrix, vector<vector<Node>>&
         Node& now = pq.top();
         pq.pop();
 
-        if (now.prevVisited) continue;
+        if (now.prevVisited) continue; //Checks if current node has already been visited
 
         now.prevVisited = true;
 
-        if (now == end) break;
+        if (now == end) break; //Checks if current node is the set end node for the function
 
-        for (int i = -1; i <= 1; i++) {
+        for (int i = -1; i <= 1; i++) { //Loops through neighboring nodes
 
             for (int j = -1; j <= 1; j++) {
 
                 if (i == 0 && j == 0) continue;
 
-                int x = now.x + i;
+                int x = now.x + i; //Updates current node
                 int y = now.y + j;
 
-                if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && adjacencyMatrix[x][y] != 0) {
+                if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && adjacencyMatrix[x][y] != 0) {  //Updates distance based on current node
 
                     Node& adj = grid[x][y];
                     double distance = now.distance + sqrt(i * i + j * j);
@@ -55,7 +59,7 @@ void dijkstra(const vector<vector<Node>>& adjacencyMatrix, vector<vector<Node>>&
 
                         adj.distance = distance;
                         adj.parent = &now;
-                        pq.push(adj);
+                        pq.push(adj); //Adds node to priority queue
 
                     }
                 }
