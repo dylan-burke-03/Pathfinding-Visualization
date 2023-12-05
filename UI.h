250c784ce95fe3ac52c681e0e2b0a5e137ff3555
dylan-burke-03 <117020610+dylan-burke-03@users.noreply.github.com>
2023-12-04 23:31:22 -0500

    Add files via upload

--- UI.h
+++ UI.h
@@ -6,22 +6,20 @@
 using namespace std;
 
 void generateGraph(int w, int h, sf::RenderWindow& window, AdjMatrix& graph, int rect_width=20) {
-    for (int i = 40; i < w-40; i+=rect_width) {
+    for (int i = 40; i < w-40; i+=rect_width) { // bounds adjusted for window 'black'space
         for (int j = 140; j < h-140; j+=rect_width) {
-            sf::RectangleShape rect; rect.setSize(sf::Vector2f(20, 20));
+            sf::RectangleShape rect; rect.setSize(sf::Vector2f(20, 20)); // construct each grid rectangle
             rect.setPosition(i, j); rect.setOutlineColor(sf::Color::Black);
             rect.setOutlineThickness(1); rect.setFillColor(sf::Color::White);
-            graph.insertNode(floor(i/20), floor(j/20), i, j, rect);
+            graph.insertNode(floor(i/20), floor(j/20), i, j, rect); // insert each grid element into the graph
             graph.drawWall(i, j);
         }
     }
 }
 
-void drawGraph(sf::RenderWindow& window, AdjMatrix& graph, int rect_width=20) {
-    for (int i = 40; i < 1920-40; i+=rect_width) {
-        for (int j = 140; j < 1080-140; j+=rect_width) {
-            //auto node = graph.getNodeElement(i, j);
-            //if (node.traversible) node.grid_element.setFillColor(sf::Color::Black);
+void drawGraph(sf::RenderWindow& window, AdjMatrix& graph, int w, int h, int rect_width=20) {
+    for (int i = 40; i < w-40; i+=rect_width) {
+        for (int j = 140; j < h-140; j+=rect_width) {
             auto node = graph.getNodeElement(i, j);
             window.draw(node.grid_element);
         }
@@ -29,7 +27,7 @@ void drawGraph(sf::RenderWindow& window, AdjMatrix& graph, int rect_width=20) {
 }
 
 void drawButtons(int w, int h, sf::RenderWindow& window, sf::Event& e, sf::Mouse& m) {
-    sf::RectangleShape start, a_star, dijkstra;
+    sf::RectangleShape start, a_star, dijkstra; // start, a_star, and dijkstra's buttons
     sf::Vector2f rect_size = sf::Vector2f(150, 50);
     sf::Font serif; serif.loadFromFile("src/serif.otf");
     sf::Text start_txt("START", serif), a_star_txt("A*", serif), dijkstra_txt("Dijkstra's", serif);
@@ -52,35 +50,6 @@ void drawButtons(int w, int h, sf::RenderWindow& window, sf::Event& e, sf::Mouse
     window.draw(start_txt);
     window.draw(a_star_txt);
     window.draw(dijkstra_txt);
-
-    bool start_clicked = false, a_star_clicked = false, dijkstra_clicked = false;
-    if (e.type == sf::Event::MouseButtonPressed) {
-        if (sf::Mouse::isButtonPressed) {
-            cout << "Clicked" << endl;
-        }
-        /*
-        if (e.mouseButton.button == m.Left) {
-            auto pos = m.getPosition(window);
-            if ((e.mouseButton.x >= 950 && e.mouseButton.x <= 1100) && (e.mouseButton.y >= 978 && e.mouseButton.y <= 1028)) {
-                start_clicked = true;
-                cout << "START" << endl;
-            }
-        }
-        
-        if ((pos.x >= 950 && pos.x <= 1100) && (pos.y >= 978 && pos.y <= 1028))
-            start_clicked = true;
-        if ((pos.x >= 400 && pos.x <= 550) && (pos.y >= 978 && pos.y <= 1028))
-
-        if (pos.y >= 978 && pos.y <= 1028) {
-            if (pos.x >= 950 && pos.x <= 1100) {
-                start_clicked = true;
-                cout << "START" << endl;
-            }
-            if (pos.x >= 400 && pos.x <= 550) a_star_clicked = true;
-            if (pos.x >= 1500 && pos.x <= 1650) dijkstra_clicked = true;
-        }
-        */
-    }
 }
 
 sf::Vector2i drawStart(sf::RenderWindow& window, AdjMatrix& graph, sf::Event& e, sf::Mouse& m, bool& start) {
@@ -112,16 +81,16 @@ sf::Vector2i drawEnd(sf::RenderWindow& window, AdjMatrix& graph, sf::Event& e, s
 }
 
 pair<AdjMatrix::node, AdjMatrix::node> generateStartEnd(sf::RenderWindow& window, AdjMatrix& graph, int x1, int y1, int x2, int y2) {
-    AdjMatrix::node& start_node = graph.getNodeElement(x1, y1);
-    AdjMatrix::node& end_node = graph.getNodeElement(x2, y2);
+    AdjMatrix::node& start_node = graph.getNodeElement(x1, y1); // gets starting graph node
+    AdjMatrix::node& end_node = graph.getNodeElement(x2, y2); // gets ending graph node
     start_node.grid_element.setFillColor(sf::Color::Green);
     end_node.grid_element.setFillColor(sf::Color::Red);
-    pair<AdjMatrix::node, AdjMatrix::node> start_end(start_node, end_node);
+    pair<AdjMatrix::node, AdjMatrix::node> start_end(start_node, end_node); // returns pair of start/end positions
     return start_end;
 }
 
 void drawBoard(int w, int h, sf::RenderWindow& window, sf::Event& e, sf::Mouse& m, AdjMatrix& graph, bool& s, bool& end) {
-    drawGraph(window, graph);
+    drawGraph(window, graph, w, h);
     drawButtons(w, h, window, e, m);
 }
 
--- graph.h
+++ graph.h
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-bool generateRandBool() {
+bool generateRandBool() { // generates random boolean value for wall generation
     static std::default_random_engine generator(std::random_device{}());
     static std::bernoulli_distribution distribution(0.85);
     return distribution(generator);
@@ -15,14 +15,14 @@ bool generateRandBool() {
 class AdjMatrix {
     public:
         struct node {
-            bool traversible, prev_visited = false; // (window_width / rect_size)
+            bool traversible, prev_visited = false; // (window_width / rect_size) 
             sf::RectangleShape grid_element;
             node* parent;
-            int x, y, weight=INT_MAX;
-            double g, h, f;
+            int x, y, weight=INT_MAX; // variables for Dijkstra's
+            double g, h, f; // variables for A*
             
-            double heuristic(const node& end) const {
-                return abs(x - end.x) + abs(y - end.y);
+            double heuristic(const node& end) const { // helper function for A*
+               return abs(x - end.x) + abs(y - end.y);
             }
 
             node() {};
@@ -31,19 +31,19 @@ class AdjMatrix {
 
         node graph[96][54];
 
-        AdjMatrix() {};
+        AdjMatrix() {}; // default constructor
 
         void insertNode(int m, int n, int x, int y, sf::RectangleShape grid_e) {
             bool trav = generateRandBool();
-            graph[m][n] = node(trav, grid_e, x, y);
+            graph[m][n] = node(trav, grid_e, x, y); // inserts node at position (m, n) in graph
         }
 
-        node& getNodeElement(int x, int y) { // from coordinates
+        node& getNodeElement(int x, int y) { // retrieves the node element from graph given coords
             int m = floor(x/20), n = floor(y/20);
             return graph[m][n];
         }
 
-        void drawWall(int x, int y) {
+        void drawWall(int x, int y) { // constructs wall on the screen if graph element is not traversible
             int m = floor(x/20), n = floor(y/20);
             if (!graph[m][n].traversible) {
                 graph[m][n].grid_element.setFillColor(sf::Color::Black);
